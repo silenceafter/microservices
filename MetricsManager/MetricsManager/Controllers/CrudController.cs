3feac3e984c4b6632752c75d58548584063249bc
Aleksandr Pinayev <a59-info@yandex.ru>
2021-05-01 19:14:08 +0300

    Задание 1

---
+++ MetricsManager/MetricsManager/Controllers/CrudController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : Controller
+    {
+        private readonly ValuesHolder _holder;
+        public ValuesHolder holder { get { return _holder; } set { } }
+
+        public CrudController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] int temperature, [FromQuery] DateTime time)
+        {
+            holder.Values.Add(new WeatherForecast() { TemperatureC = temperature, Date = time });
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(holder.Values);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime time, [FromQuery] int newTemperature)
+        {
+            for (int i = 0; i < holder.Values.Count; i++)
+            {
+                if (holder.Values[i].Date == time)
+                    holder.Values[i] = new WeatherForecast() { TemperatureC = newTemperature, Date = time };
+            }
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime timeFrom, [FromQuery] DateTime timeTo)
+        {
+            holder.Values = holder.Values.Where(w => w.Date < timeFrom || w.Date > timeTo).ToList();                  
+            return Ok();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MetricsManager/MetricsManager/ValuesHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager
+{
+    public class ValuesHolder
+    {
+        public List<WeatherForecast> Values { get; set; }   
+        public ValuesHolder()
+        {
+            Values = new List<WeatherForecast>();
+        }
+    }
+}
