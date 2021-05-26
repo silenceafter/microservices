e0b5a802efdf9513f9c052a9ed1d4cff4610aafa
Aleksandr Pinayev <a59-info@yandex.ru>
2021-05-27 02:27:58 +0300

    MetricsManager
    FluentMigrator, Quartz.NET

---
+++ MetricsManager/MetricsAgent/DAL/Repositories/DotNetMetricsRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using MetricsAgent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace MetricsAgent.DAL.Repositories
+{
+    public class DotNetMetricsRepository : IDotNetMetricsRepository
+    {
+        private readonly ISqlConnectionProvider _provider;
+        public DotNetMetricsRepository(ISqlConnectionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void Create(DotNetMetric item)
+        {
+            var ConnectionString = _provider.GetConnectionString();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Execute("INSERT INTO dotnetmetrics(value, time) VALUES(@value,@time)",
+                new
+                {
+                    value = item.Value,
+                    time = item.Time
+                });
+            }
+        }
+
+        public List<DotNetMetric> GetByTimePeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
+        {
+            var ConnectionString = _provider.GetConnectionString();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                return connection.Query<DotNetMetric>("SELECT * FROM dotnetmetrics WHERE time >= @fromTime AND time <= @toTime",
+                    new
+                    {
+                        fromTime = fromTime.ToUnixTimeSeconds(),
+                        toTime = toTime.ToUnixTimeSeconds()
+                    }).ToList();
+            }            
+        }
+    }
+}
---
+++ MetricsManager/MetricsAgent/DAL/SqlConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.DAL
+{
+    public class SqlConnection : ISqlConnectionProvider
+    {
+        private const string _connectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;";
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
